46b2822f02192223192d647ad675fc19cee324a8
ItzArima <skaypaprilia@gmail.com>
2022-01-15 14:34:48 +0100

    random trip display in home page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,10 +4,41 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Model\Flight;
+
+use DB;
+
 class PageController extends Controller
 {
     public function home(){
-        return view('home');
+        $flights = Flight::all();
+        $random = [];
+        if(count($flights) > 0){
+            for($i = 0; $i<4; $i++){
+                $num = rand(0,count($flights)-1);
+                if(!in_array($flights[$num], $random)){
+                    array_push($random, $flights[$num]);
+                }
+                else{
+                    $i--;
+                }
+            }
+        }
+        $images=[
+            [
+                'id' => 1011,
+            ],
+            [
+                'id' => 1039,
+            ],
+            [
+                'id' => 122,
+            ],
+            [
+                'id' => 158,
+            ],
+        ];
+        return view('home' , compact('random', 'images'));
     }
 
     public function dashboard(){
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,31 @@
             <div class="text-container">
                 <h1>Trips for the life</h1>
             </div>
+            <div class="suggested-container">
+                <h1>Suggested Trips</h1>
+                <div class="cards-container">
+                    @foreach($random as $key => $card)                  
+                        <div class="card">
+                            <div class="image">
+                                <img src="https://picsum.photos/id/{{$images[$key]['id']}}/300/200" alt="">
+                            </div>  
+                            <div class="departure">
+                                <h2>From: {{$card->departure}}</h2>
+                            </div>
+                            <div class="destination">
+                                <h2>To: {{$card->destination}}</h2>
+                            </div>
+                            <div class="price">
+                                <p>{{$card->price}} EUR</p>
+                            </div>
+                            <div class="datetime">
+                                <p>Date and Time</p>
+                                <p>{{$card->date}}, {{$card->time}}</p>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
         </div>
     </main>
 @endsection
