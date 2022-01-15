b0381e549000a1c5a9eb604bc0cbf8d766cc3ff9
ItzArima <skaypaprilia@gmail.com>
2022-01-15 19:59:19 +0100

    debug on home header routing

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PageController@home')->name('home');
 
+Route::get('/home', 'PageController@home')->name('home');
+
 Route::resource('flight' , 'FlightController');
 
 Route::get('dashboard', 'PageController@dashboard')->name('dashboard');
